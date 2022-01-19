6faa57cad60042a53e70628b4651762878457c33
Dickson Yu <dickson.yu@email.com>
2022-01-18 20:00:09 -0500

    Adding daylistitem test and fixing errors from the test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,7 +3,19 @@ import React from 'react';
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formalSpots = spots => {
+  if (!spots) {
+    return 'no spots remaining';
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+  return `${spots} spots remaining`;
+}
+
 export default function DayListItem(props) {
+  const spotMessage = formalSpots(props.spots)
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
@@ -12,7 +24,7 @@ export default function DayListItem(props) {
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text-regular">{props.name}</h2>
-      <h3 className="text-light">{props.spots} spots remaining</h3>
+      <h3 className="text-light">{spotMessage}</h3>
     </li>
   );
 }
