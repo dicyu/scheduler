2c2cc8a0aadb038cefb78b761cd8d7e1a6668910
Dickson Yu <dickson.yu@email.com>
2022-01-21 16:14:38 -0500

    Adding grabbing days data from api using axios, deleting hardcoded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
-
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
 
@@ -45,34 +45,26 @@ const appointments = [
   }
 ];
 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appSchedule = appointments.map((scheduleObj) => {
   return (
     <Appointment key={scheduleObj.id} {...scheduleObj} />
     )
   });
-  
 
+  
   export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+    const [days, setDays] = useState([]);
+    
+    // API Axios call for days
+    useEffect(() => {
+      const daysURL = 'http://localhost:8001/api/days';
+      axios.get(daysURL)
+        .then((response) => {
+          console.log(response.data)
+          setDays([...response.data])
+        });
+    }, []);
+
 
   return (
   <main className="layout">
@@ -86,8 +78,8 @@ const appSchedule = appointments.map((scheduleObj) => {
       <nav className='sidebar__menu'>
         <DayList
           days={days}
-          value={day}
-          onChange={setDay}
+          value={days}
+          onChange={setDays}
         />
       </nav>
       <img
