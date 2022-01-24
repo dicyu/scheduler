76f6deb8e4d47b3e0a8757c9b99f47a516188643
Dickson Yu <dickson.yu@email.com>
2022-01-24 15:07:35 -0500

    Using our new visualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,21 +3,41 @@ import React from "react";
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from "./Form";
+
+import useVisualMode from 'hooks/useVisualMode';
 
 
 import "components/Appointment/styles.scss";
 import { action } from "@storybook/addon-actions/dist/preview";
 
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
 function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY 
+  );
+
   return (
     <article className='appointment'>
       <Header time={props.time} />
-      {props.interview ? <Show 
+      {mode === EMPTY && <Empty onAdd={() => {transition(CREATE)}} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={back}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
         onEdit={action("onEdit")}
         onDelete={action("onDelete")}
-        /> : <Empty />}
+        />
+      )}
     </article>
   );
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    if (replace) {
+      setMode(mode);
+    } else {
+      setMode(mode);
+      setHistory([...history, mode]);
+    }
+  }
+
+  const back = () => {
+    if (history.length === 1) {
+      setMode(initial);
+    } else {
+      setMode(history[history.length  - 2]);
+      setHistory(history.slice(0, -1));
+    }
+  }
+
+  return { mode, transition, back };
+}
